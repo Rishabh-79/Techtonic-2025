e1e6b774294e1ce88e664aa0fa5e3ce7b0911322
Rishabh .M <59rishabh@gmail.com>
2025-08-16 12:10:02 +0530

    Updated the database and the form

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask,request,render_template,redirect,send_file
+from flask import Flask,request,render_template,redirect,send_file,flash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime,timezone,timedelta
 import os
 from flask_mail import Mail,Message
 import pandas as pd
@@ -8,48 +8,50 @@ import io
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 instance_dir = os.path.join(base_dir, 'instance')
+IST=timezone(timedelta(hours=5,minutes=30))
 
 app=Flask(__name__,instance_path=instance_dir)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Reg.db'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://registrationdb_1xph_user:4VVgNFIQc0TUQxlxJk0GM97TCkayQ7J3@dpg-d2fq81a4d50c73b53lf0-a/registrationdb_1xph'
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=587,
     MAIL_USERNAME=''
-
 )
+
 db=SQLAlchemy(app)
 
 class Todo(db.Model):
     SNo=db.Column(db.Integer,primary_key=True)
     S1_name=db.Column(db.String(200),nullable=False)
+    Age1=db.Column(db.String(200),nullable=False)
+    S2_name=db.Column(db.String(200),nullable=True)
+    Age2=db.Column(db.String(200),nullable=True)
     Clg_name=db.Column(db.String(200),nullable=False)
-    Age=db.Column(db.String(200),nullable=False)
-    Event_ID=db.Column(db.String(200),nullable=False)
-    CDate=db.Column(db.DateTime,default=datetime.utcnow())
+    Event=db.Column(db.String(200),nullable=False)
+    CDate=db.Column(db.DateTime,default=datetime.now(IST))
 
 
-def OrmToList(reg_data):
-    l=[]
-    for i in reg_data:
-        l.append([i.SNo,i.S1_name,i.Clg_name,i.Age,i.Event_ID,i.CDate])
-    return l
-
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method=="POST":
-        Sn=request.form['Name']
+        Sn1=request.form['Name1']
+        SAge1=request.form['Age1']
+        Sn2=request.form['Name2']
+        SAge2=request.form['Age2']
         College=request.form['Clg']
-        SAge=request.form['Age']
-        E_id=request.form['Eid']
-        Reg_new=Todo(S1_name=Sn,Clg_name=College,Age=SAge,Event_ID=E_id)
+        SEvent=request.form['Event']
+        Reg_new=Todo(S1_name=Sn1,Age1=SAge1,S2_name=Sn2,Age2=SAge2,Clg_name=College,Event=SEvent)
         db.session.add(Reg_new)
         db.session.commit()
         return redirect('/registration')
     else:
         return render_template('index.html')
     
+
 @app.route('/registration',methods=['POST','GET'])
 def dlt():
     if request.method=="POST":
@@ -57,10 +59,12 @@ def dlt():
     else:
         return render_template("RegSuccess.html")
 
+
 @app.route('/admin/database')
 def dba():
     query=db.session.query(Todo).all()
     return render_template('test.html', lists=query)
+
 
 @app.route('/download7798')
 def dwnld():
@@ -88,7 +92,6 @@ def dwnld():
         mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
     
-
 
 if __name__=='__main__':
     app.run(debug=True)
